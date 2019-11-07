dc934fdd37dbf08187043ff29938a65641364ee5
Mathys Ballagny <mathys.b@free.fr>
2019-11-07 12:19:22 +0100

    Fin du Word (-sommaire)

--- src/main/java/edu/xml/app/ModelWord.java
+++ src/main/java/edu/xml/app/ModelWord.java
@@ -1,20 +1,23 @@
 package edu.xml.app;
 
-import java.io.File;
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
@@ -31,8 +34,6 @@ public class ModelWord {
     public ModelWord(Bibliotheque bibliotheque, String filename) {
         this.bibliotheque = bibliotheque;
         this.filename = filename;
-        buildModel();
-        exportModel("/home/mathys/Cours/Prog Avancée/test.docx");
     }
 
     public void exportModel(String filepath) {
@@ -84,32 +85,133 @@ public class ModelWord {
         sommaire.setPageBreak(true);
         sommaire.setAlignment(ParagraphAlignment.CENTER);
 
-        document.createTOC();
+        // document.createTOC();
 
-        XWPFParagraph dummy = document.createParagraph();
-        dummy.setPageBreak(true);
 
         List<Livre> livres = sortBiblio();
 
+
+        XWPFParagraph listeLivres = document.createParagraph();
+        listeLivres.setPageBreak(true);
+        XWPFRun listeLivresRun = listeLivres.createRun();
+        listeLivresRun.setFontSize(30);
+        listeLivresRun.setText("Livres:");
+        listeLivres.setStyle("Heading1");
+        listeLivres.setAlignment(ParagraphAlignment.CENTER);
+
         String actualAuthor = "";
+        XWPFParagraph author = null;
+        XWPFRun authorRun = null;
+        XWPFTable livreTable = null;
+        XWPFTableRow livreRow = null;
+        XWPFTableCell livreCell = null;
+        XWPFParagraph livreDisp = null;
+        XWPFRun livreRun = null;
+        XWPFParagraph imgDisp = null;
+        XWPFRun imgRun = null;
         for(Livre livre: livres){
             try{
-                if(actualAuthor != livre.getAuteur().getPrenom() + " " + livre.getAuteur().getNom()){
-
+                if(!actualAuthor.equals(livre.getAuteur().getPrenom() + " " + livre.getAuteur().getNom()) ){
+                    actualAuthor = livre.getAuteur().getPrenom() + " " + livre.getAuteur().getNom();
+                    author = document.createParagraph();
+                    authorRun = author.createRun();
+                    authorRun.setText(actualAuthor);
+                    authorRun.setFontSize(20);
+                    authorRun.setBold(true);
+                    author.setAlignment(ParagraphAlignment.CENTER);
+                    author.setStyle("Heading2");
                 }
-                XWPFParagraph livreDisp = document.createParagraph();
-                XWPFRun livreRun = livreDisp.createRun();
+                livreTable = document.createTable();
+                livreTable.removeBorders();
+                livreRow = livreTable.getRow(0);
+                livreCell = livreRow.getCell(0);
+                imgDisp = livreCell.getParagraphArray(0);
+                imgRun = imgDisp.createRun();
                 String url = livre.getImgUrl();
                 //InputStream is = new URL(url).openStream();
-                FileInputStream is = new FileInputStream(url);
-                livreRun.addPicture(is, XWPFDocument.PICTURE_TYPE_JPEG, url, Units.toEMU(200), Units.toEMU(200)); // 200x200 pixels
-                is.close();
-                livreRun.setText("");
+                if(url != null){
+                    FileInputStream imageStream = new FileInputStream(url);
+                    BufferedImage img = ImageIO.read(imageStream);
+                    double w = img.getWidth();
+                    double h = img.getHeight();
+                    imageStream.close();
+                    FileInputStream is = new FileInputStream(url);
+                    double ratio = Math.floor(h / 100);
+                    h = h /ratio;
+                    w = w / ratio;
+                    imgRun.addPicture(is, XWPFDocument.PICTURE_TYPE_JPEG, url, Units.toEMU(w), Units.toEMU(h)); // 200x200 pixels
+                    is.close();
+                    livreCell = livreRow.addNewTableCell();
+                }
+                else{
+                    livreCell = livreRow.getCell(0);
+                }
+                livreDisp = livreCell.getParagraphArray(0);
+                livreRun = livreDisp.createRun();
+                livreRun.setText(livre.getTitre());
+                livreRun.setFontSize(14);
                 livreRun.addBreak();
+                livreRun.setText(String.valueOf(livre.getParution()));
             }
             catch(Exception e){
-                e.printStackTrace();
+                // e.printStackTrace();
+                livreCell = livreRow.getCell(0);
+                livreDisp = livreCell.getParagraphArray(0);
+                livreRun = livreDisp.createRun();
+                livreRun.setText(livre.getTitre());
+                livreRun.setFontSize(14);
+                livreRun.addBreak();
+                livreRun.setText(String.valueOf(livre.getParution()));
             }
+        }
+        
+        XWPFParagraph pretStatus = document.createParagraph();
+        pretStatus.setPageBreak(true);
+        XWPFRun pretStatusRun = pretStatus.createRun();
+        pretStatusRun.setFontSize(30);
+        pretStatusRun.setText("Prêts:");
+        pretStatus.setStyle("Heading1");
+        pretStatus.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFTable pretTable = document.createTable();
+        XWPFTableRow pretRow = pretTable.getRow(0);
+        XWPFTableCell pretCell = pretRow.getCell(0);
+        XWPFParagraph cellParagraph = pretCell.getParagraphArray(0);
+        XWPFRun cellText = cellParagraph.createRun();
+        cellText.setBold(true);
+        cellText.setText("Livre");
+        pretCell = pretRow.addNewTableCell();
+        cellParagraph = pretCell.getParagraphArray(0);
+        cellText = cellParagraph.createRun();
+        cellText.setBold(true);
+        cellText.setText("Prêté?");
+        pretCell = pretRow.addNewTableCell();
+        cellParagraph = pretCell.getParagraphArray(0);
+        cellText = cellParagraph.createRun();
+        cellText.setBold(true);
+        cellText.setText("Acquis par");
+        
+        for(Livre livre: livres){
+            pretRow = pretTable.createRow();
+            pretCell = pretRow.getCell(0);
+            cellParagraph = pretCell.getParagraphArray(0);
+            cellText = cellParagraph.createRun();
+            cellText.setText(livre.getTitre());
+            pretCell = pretRow.getCell(1);
+            cellParagraph = pretCell.getParagraphArray(0);
+            cellText = cellParagraph.createRun();
+            boolean pret = livre.isPret();
+            if(pret){
+                cellText.setText("Oui");
+            }
+            else{
+                cellText.setText("Non");
+            }
+            pretCell = pretRow.getCell(2);
+            cellParagraph = pretCell.getParagraphArray(0);
+            cellText = cellParagraph.createRun();
+            cellText.setText(livre.getAcquis());
+            
         }
     }
 
